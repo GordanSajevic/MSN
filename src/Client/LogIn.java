263c573591b0c84e51d9aef72b176448bf730d2d
Gordan Sajević <gsajevic@hotmail.com>
2015-02-08 10:47:50 +0100

    Update LogIn.java

--- src/Client/LogIn.java
+++ src/Client/LogIn.java
@@ -1,9 +1,11 @@
 package Client;
 
-import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -24,6 +26,8 @@ public class LogIn {
 	private JPasswordField pass;
 	private String host;
 	private int port;
+	private OutputStream os;
+	private static InputStream is;
 
 	public LogIn(String host, int port) {
 		this.host = host;
@@ -72,7 +76,8 @@ public class LogIn {
 
 
 
-	private class ButtonHandler extends KeyAdapter implements ActionListener {
+	private class ButtonHandler extends KeyAdapter implements ActionListener 
+	{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -96,8 +101,8 @@ public class LogIn {
 			Socket client;
 			try {
 				client = new Socket(host, port);
-				OutputStream os  = client.getOutputStream();
-				InputStream is = client.getInputStream();
+				os  = client.getOutputStream();
+				is = client.getInputStream();
 				os.write((username + "\n").getBytes());
 				os.write((password + "\n").getBytes());
 				
@@ -107,9 +112,20 @@ public class LogIn {
 				
 				ChatGui gui = new ChatGui(client);
 				new Thread(gui).start();
-				} else {
-					showError("Username i password nije tacan");
+				} 
+				else if (result == -1) 
+				{
+					showError("Username nije tačan");
 				}
+				else if (result == -2)
+				{
+					showError("Password nije tačan");
+				}
+				else
+				{
+					showError("Ni username ni password nisu tačni");
+				}
+				
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
@@ -117,5 +133,31 @@ public class LogIn {
 
 		}
 
+	}
+	
+	public static void receiveFile(File file)
+	{
+		try {
+			
+			byte[] byteArray = new byte[(int)file.length()];
+			FileOutputStream fos = new FileOutputStream(file);
+			BufferedOutputStream bos = new BufferedOutputStream(fos);
+			int bytesRead = is.read(byteArray, 0, byteArray.length);
+			int current = bytesRead;
+			do
+			{
+				bytesRead = is.read(byteArray, current, byteArray.length - current);
+				if (bytesRead >= 0)
+				{
+					current += bytesRead;
+				}
+			}while(bytesRead > -1);
+			bos.write(byteArray, 0, current);
+			bos.flush();
+			System.out.println("File primljen (" + current + " bajtova)");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
